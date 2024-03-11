17fd8395ed4777fccece3c55b280581b8c7a5d73
lanhf <nghoangvu24@gmail.com>
2024-03-12 00:08:59 +0700

    create repo and initialize with cookiecutter-django

---
+++ pdf_translator/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pdf_translator.users.models import User
+from pdf_translator.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
